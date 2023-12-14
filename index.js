faec76c240ad4ddb149bd252c97dea73e9a60f91
KinshasaPolee <kpolee1@gmail.com>
2023-12-14 06:13:13 -0500

    created buildSubmenu function

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 // Menu Links
 var menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '#', subLinks: [
-    {text: 'all', href: '/catalog/all'},
-    {text: 'top selling', href: '/catalog/top'},
-    {text: 'search', href: '/catalog/search'},
-    ]},
-    {text: 'orders', href: '#' , subLinks: [
-    {text: 'new', href: '/orders/new'},
-    {text: 'pending', href: '/orders/pending'},
-    {text: 'history', href: '/orders/history'},
-    ]},
-    {text: 'account', href: '#', subLinks: [
-    {text: 'profile', href: '/account/profile'},
-    {text: 'sign out', href: '/account/signout'},
-    ]},
+    { text: 'about', href: '/about' },
+    {
+        text: 'catalog', href: '#', subLinks: [
+            { text: 'all', href: '/catalog/all' },
+            { text: 'top selling', href: '/catalog/top' },
+            { text: 'search', href: '/catalog/search' },
+        ]
+    },
+    {
+        text: 'orders', href: '#', subLinks: [
+            { text: 'new', href: '/orders/new' },
+            { text: 'pending', href: '/orders/pending' },
+            { text: 'history', href: '/orders/history' },
+        ]
+    },
+    {
+        text: 'account', href: '#', subLinks: [
+            { text: 'profile', href: '/account/profile' },
+            { text: 'sign out', href: '/account/signout' },
+        ]
+    },
 ];
 
 for (var link of menuLinks) {
@@ -28,51 +34,23 @@ newElt.setAttribute('Link Text:', 'link.text');
 document.topMenuEl.appendChild(newElt);
 // End of Menu Links
 let main = document.getElementById('mainEL');
-if(main) {
+if (main) {
     main.classList.add('flex-ctr');
 } else {
     console.error('Element with id "mainEl" not found.');
 }
 
 let topMenuEl = document.getElementById('top-menu');
-if(topMenuEl) {
+if (topMenuEl) {
     topMenuEl.style.height = '100%';
     topMenuEl.classList.add('flex-around');
     var backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--top-menu-bg').trim();
 } else {
     console.error('Element with id "topMenuEl" not found.');
 }
-
-// let topMenuLinks = document.querySelectorAll('a');
-
-topMenuEl.addEventListener('click', function(event) {
-    event.preventDefault();
-
-    if(event.target.tagName === 'a') {
-        const clickedLink = event.target;
-    console.log(event.target.textContent);
-
-        const isAlreadyActive = clickedLink.classList.contains('active');
-
-        const topMenuLinks = topMenuEl.querySelectorAll('a');
-        topMenuLinks.forEach(link => {
-            if(link !== clickedLink) {
-                link.classList.remove('active');
-            }
-        });
-
-        if(!isAlreadyActive) {
-            clickedLink.classList.add('active');
-        } else {
-            clickedLink.classList.remove('active');
-        }
-        // } else {
-        // return;
-    }
-});
-
+// sub Menu
 let subMenuEl = document.getElementById('sub-menu');
-if(subMenuEl) {
+if (subMenuEl) {
     subMenuEl.style.height = '100%';
     subMenuEl.classList.add('flex-around');
     subMenuEl.style.position = 'absolute';
@@ -81,3 +59,47 @@ if(subMenuEl) {
 } else {
     console.error('Element with id "subMenuEl" not found.');
 }
+
+// let topMenuLinks = document.querySelectorAll('a');
+function buildSubmenu(subLinks) {
+    subMenuEl.innerHTML = '';
+    subLinks.forEach(linkObject => {
+        const subLinkElement = document.createElement('a');
+        subLinkElement.setAttribute('href', linkObject.href);
+        subLinkElement.textContent = linkObject.text;
+        subMenuEl.appendChild(subLinkElement);
+    });
+}
+topMenuEl.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    if (event.target.tagName === 'a') {
+        const clickedLink = event.target;
+        console.log(event.target.textContent);
+        const linkObject = getLinkObject(clickedLink);
+
+        const isAlreadyActive = clickedLink.classList.contains('active');
+
+        const topMenuLinks = topMenuEl.querySelectorAll('a');
+        topMenuLinks.forEach(link => {
+            if (link !== clickedLink) {
+                link.classList.remove('active');
+            }
+        });
+
+        if (!isAlreadyActive) {
+            clickedLink.classList.add('active');
+
+            if (linkObject.subLinks) {
+                subMenuEl.style.top = '100%';
+                buildSubmenu(linkObject.subLinks);
+            } else {
+                subMenuEl.style.top = '0';
+            }
+        } else {
+            clickedLink.classList.remove('active');
+        }
+        console.log(clickedLink.textContent);
+    }
+});
+// Do Not forget to add 1-5 requirements right before Part 6
